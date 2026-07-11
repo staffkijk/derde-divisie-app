f5588f4019b273bde8507979995b2db69204ac3a
staffkijk <ngijs@icloud.com>
2026-07-11 22:11:37 +0200

    Gebruik geoptimaliseerde webversie van introfilm

--- lib/core/config/media_config.dart
+++ lib/core/config/media_config.dart
@@ -2,6 +2,6 @@ class MediaConfig {
   const MediaConfig._();
 
   static final Uri introVideo20262027Url = Uri.parse(
-    'https://firebasestorage.googleapis.com/v0/b/derde-divisie-app.firebasestorage.app/o/videos%2Fderdediv_intro_2026_2027_web.mp4?alt=media&token=cee2170e-8ed2-4247-99fb-9642d11b880e',
+    'https://firebasestorage.googleapis.com/v0/b/derde-divisie-app.firebasestorage.app/o/videos%2Fderdediv_intro_2026_2027_web_v2.mp4?alt=media&token=e4138164-5af2-420c-85fb-4b4a440aced1',
   );
 }
